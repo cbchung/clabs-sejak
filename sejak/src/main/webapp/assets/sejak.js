4e7453283bed3708526887bb0d5d28d9dbf654f2
changbae chung <changbae.chung@gmail.com>
2016-01-08 05:16:58 +0900

    some

--- sejak/src/main/webapp/assets/sejak.js
+++ sejak/src/main/webapp/assets/sejak.js
@@ -60,11 +60,11 @@ Sejak = {
 		this.tools.initPageTags();
 	},
 	ctx : {
-		modules : [],
+		pools : [],
 		makeModuleElement : function(name){
-			var moduleE = $("<module id='"+ name +"' loaded='tring'/>");
+			var moduleE = $("<module id='sejak-app-module-id-"+ name +"' loaded='tring'/>");
 			$("sejak-app").append(moduleE);
-			this.modules.push(moduleE);
+//			this.modules.push(moduleE);
 			return moduleE;
 		},
 		moduleCallBack : function(rc, el, data){
@@ -74,15 +74,47 @@ Sejak = {
 			}
 			else $(el).attr('loaded', 'fail');
 		},
-		loadContext : function(pack, module){
-			for(var key in Sejak.Project.contexts){
-				var ctx = Sejak.Project.contexts[key];
-				if(ctx[0] == (pack+"-"+module)){
-					console.log('OK-find package : ' + JSON.stringify(ctx));
-					return;
+		loadContext : function(pack, module, el){
+			var cntl = $('#sejak-app-module-id-'+pack).find("cntl[name='"+module+"']");
+			var view = $('#sejak-app-module-id-'+pack).find(module);
+			
+			
+			console.log('cntl:'+ $(cntl).text());
+			console.log('view:'+ $(view).html());
+			if($(cntl).text() == undefined || $(view).html() == undefined) return;
+			
+			var ctx = {
+				e : $(el),
+				scope : {},
+				view : $(view).html(),
+				cntl : eval("cntl= function(){ var ctx=this.scope, el=this.element; "+$(cntl).text()+"}"),
+				element : function(sel){
+					var html = $(this.e).html();
+					console.log('e-111111111111');
+					console.log(html);
+					$(this.e).find("[name='navbar']").css('border', 'solid 1px blue');
+					console.log('e-22222222222');
+					return $(this.e).find(sel);
+				},
+				onData : function(data){ this.scope.onData(data); this.execute(); },
+				execute : function(){
+//					var content = $(view).html();
+					var content = this.view;
+					var results = content.match(/\{{\s*\w+\s*}}/g);
+					for(var i in results){
+						var v = results[i].replace("{{", "").replace("}}","").trim();
+						content=content.replace(results[i], this.scope[v]);
+					}
+					console.log($(this.e).html());
+					$(this.e).html(content);
+					console.log($(this.e).html());
 				}
-			}
-			console.log("Can't-find package : " + pack+"-"+module);
+			};
+			console.log('kkkkkkkk:'+ JSON.stringify(ctx));
+			this.pools.push(ctx);
+			ctx.cntl();
+			ctx.execute();
+			ctx.onData({project:'test'});
 		}
 	},
 	tools : {
@@ -96,14 +128,23 @@ Sejak = {
 			$("[sj-model]").each(function(index){
 				console.log('js-model='+$(this).attr('sj-model'));
 				var model = $(this).attr('sj-model');
+				/*
 				var idx = model.lastIndexOf('-');
 				var packageName = (idx == -1) ? 'app' : model.substring(0, idx);
 				var moduleName = (idx == -1) ? model : model.substring(idx+1);
 				console.log('p/n='+packageName + "/" + moduleName);
 				$(this).attr('data-model-package', packageName);
 				$(this).attr('data-model-name', moduleName);
-
-				Sejak.ctx.loadContext(packageName, moduleName);
+				Sejak.ctx.loadContext(packageName, moduleName, $(this));
+				*/
+				var ctx = Sejak.Project.contexts[model];
+				if(ctx != undefined){
+					console.log('1111'+JSON.stringify(ctx));
+					var idx = model.lastIndexOf('-');
+					var packageName = (idx == -1) ? 'app' : model.substring(0, idx);
+					var moduleName = (idx == -1) ? model : model.substring(idx+1);
+					Sejak.ctx.loadContext(packageName, moduleName, $(this));
+				}
 			});
 		},
 		loadCSS : function(href) {
